ef3c9bd28deee280c408f1d99b9616caa3afb05f
ajipranomocirebon <ajiamel732021@gmail.com>
2026-08-07 08:01:04 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'petadonor-cache-v11';
+const CACHE_NAME = 'petadonor-cache-v12';
 const ASSETS = [
   './index.html', './manifest.json', './icon-192.png', './icon-512.png',
   './css/styles.css',
